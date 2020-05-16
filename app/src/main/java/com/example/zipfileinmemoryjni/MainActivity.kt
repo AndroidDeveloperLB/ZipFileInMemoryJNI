3f8202033f681f828511b0db13b287dfde036a20
AndroidDeveloperLB <AndroidDeveloperLB5020914>
2020-05-16 19:04:07 +0300

    Updated sample to show issues of parsing using SeekableInUriByteChannel

--- app/src/main/java/com/example/zipfileinmemoryjni/MainActivity.kt
+++ app/src/main/java/com/example/zipfileinmemoryjni/MainActivity.kt
@@ -21,6 +21,8 @@ import kotlin.concurrent.thread
 class MainActivity : AppCompatActivity() {
     private val numberFormat = NumberFormat.getInstance()
 
+    data class ParsingResult(val entriesAndSizes: ArrayList<Pair<String, Long>>, val timeTaken: Long) {}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -29,8 +31,8 @@ class MainActivity : AppCompatActivity() {
         thread {
             Log.d("AppLog", "started testing")
 
-//            val packageInfo = packageManager.getPackageInfo("com.google.android.configupdater", 0)
-//            val file = File(packageInfo.applicationInfo.publicSourceDir)
+            val packageInfo = packageManager.getPackageInfo("com.google.android.apps.wellbeing", 0)
+            val file = File(packageInfo.applicationInfo.publicSourceDir)
 //            val applicationInfoWithLargestApkFromInstalledApps = getApplicationInfoWithLargestApkFromInstalledApps(this)
 //            Log.d("AppLog", "packageName of APK to test:${applicationInfoWithLargestApkFromInstalledApps.packageName}")
 //            val file = File(applicationInfoWithLargestApkFromInstalledApps.publicSourceDir)
@@ -41,7 +43,7 @@ class MainActivity : AppCompatActivity() {
 //                val file = File("/storage/emulated/0/tiny.zip")
 //            val file = File("/storage/emulated/0/base.apk")
 
-//            val uri = Uri.fromFile(file)
+            val uri = Uri.fromFile(file)
 //            val fileSize = file.length()
 //            Log.d("AppLog", "will parse the file: ${file.absolutePath} fileSize:${numberFormat.format(fileSize)} ")
 //            parseUsingAndroidZipFile(file)
@@ -61,22 +63,46 @@ class MainActivity : AppCompatActivity() {
 //            val startTime = System.currentTimeMillis()
 //            var errorsCount = 0
 //            var successCount=0
+            var issuesFound = 0
+
             packageManager.getInstalledApplications(0).forEach { applicationInfo ->
-                Log.d("AppLog", "packageName of APK to test:${applicationInfo.packageName}")
+//                Log.d("AppLog", "packageName of APK to test:${applicationInfo.packageName}")
                 val file = File(applicationInfo.publicSourceDir)
 //                parseUsingJniByteArray(file).let { succeeded -> if (!succeeded) ++errorsCount }
                 val uri = Uri.fromFile(file)
 //                parseUsingSeekableInUriByteChannel(uri).let { succeeded -> if (!succeeded) ++errorsCount else ++successCount }
 //                parseUsingInefficientSeekableInUriByteChannel(uri).let { succeeded -> if (!succeeded) ++errorsCount else ++successCount }
-                parseUsingSeekableInUriByteChannel(uri)
-                parseUsingInefficientSeekableInUriByteChannel(uri)
+//                val parseUsingAndroidZipFileResult = parseUsingAndroidZipFile(file)
+                val parseUsingSeekableInUriByteChannelResult = parseUsingSeekableInUriByteChannel(uri)
+//                val parseUsingInefficientSeekableInUriByteChannelResult = parseUsingInefficientSeekableInUriByteChannel(uri)
+                if(parseUsingSeekableInUriByteChannelResult==null){
+                    Log.d("AppLog", "error: couldn't hanlde file:$file")
+                }
+//                if (parseUsingSeekableInUriByteChannelResult != null && parseUsingInefficientSeekableInUriByteChannelResult != null) {
+//                    if (parseUsingAndroidZipFileResult != null) {
+//                        if (parseUsingAndroidZipFileResult.entriesAndSizes.size != parseUsingSeekableInUriByteChannelResult.entriesAndSizes.size) {
+//                            Log.d("AppLog", "parseUsingAndroidZipFileResult:${parseUsingAndroidZipFileResult.entriesAndSizes.size} vs  ${parseUsingAndroidZipFileResult.entriesAndSizes.size}")
+//                            ++issuesFound
+//                        }
+//                        if (parseUsingAndroidZipFileResult.entriesAndSizes.size != parseUsingInefficientSeekableInUriByteChannelResult.entriesAndSizes.size) {
+//                            Log.d("AppLog", "parseUsingInefficientSeekableInUriByteChannelResult:${parseUsingInefficientSeekableInUriByteChannelResult.entriesAndSizes.size} vs ${parseUsingAndroidZipFileResult.entriesAndSizes.size}")
+//                            ++issuesFound
+//                        }
+//                    }
+//                    if (parseUsingSeekableInUriByteChannelResult.entriesAndSizes.size != parseUsingInefficientSeekableInUriByteChannelResult.entriesAndSizes.size) {
+//                        Log.d("AppLog", "different entries sizes: $parseUsingSeekableInUriByteChannelResult vs $parseUsingInefficientSeekableInUriByteChannelResult")
+//                        ++issuesFound
+//                    }
+//                }
             }
+
 //            Log.d("AppLog", "done parsing in ${System.currentTimeMillis() - startTime}ms errorsCount:$errorsCount successCount:$successCount")
+            Log.d("AppLog", "done issuesFound:$issuesFound")
         }
     }
 
-    private fun parseUsingAndroidZipFile(file: File): Boolean {
-        Log.d("AppLog", "testing file using direct path - Android framework ZipFile")
+    private fun parseUsingAndroidZipFile(file: File): ParsingResult? {
+//        Log.d("AppLog", "testing file using direct path - Android framework ZipFile")
         try {
             val startTime = System.currentTimeMillis()
             java.util.zip.ZipFile(file).use { zipFile: java.util.zip.ZipFile ->
@@ -85,21 +111,21 @@ class MainActivity : AppCompatActivity() {
                     val name = entry.name
                     val size = entry.size
                     entriesNamesAndSizes.add(Pair(name, size))
-                    Log.v("Applog", "entry name: $name - ${numberFormat.format(size)}")
+//                    Log.v("Applog", "entry name: $name - ${numberFormat.format(size)}")
                 }
-                val endTime = System.currentTimeMillis()
-                Log.d("AppLog", "got ${entriesNamesAndSizes.size} entries data. time taken: ${endTime - startTime}ms")
-                return true
+                val timeTaken = System.currentTimeMillis() - startTime
+//                Log.d("AppLog", "got ${entriesNamesAndSizes.size} entries data. time taken: $timeTaken ms")
+                return ParsingResult(entriesNamesAndSizes, timeTaken)
             }
         } catch (e: Throwable) {
-            Log.e("AppLog", "error while trying to parse using  Android framework ZipFile:$e")
+//            Log.e("AppLog", "error while trying to parse using  Android framework ZipFile:$e")
             e.printStackTrace()
         }
-        return false
+        return null
     }
 
-    private fun parseUsingApacheZipFile(file: File): Boolean {
-        Log.d("AppLog", "testing file using direct path - Apache ZipFile)")
+    private fun parseUsingApacheZipFile(file: File): ParsingResult? {
+//        Log.d("AppLog", "testing file using direct path - Apache ZipFile)")
         try {
             val startTime = System.currentTimeMillis()
             ZipFile(file).use { zipFile: ZipFile ->
@@ -108,22 +134,22 @@ class MainActivity : AppCompatActivity() {
                     val name = entry.name
                     val size = entry.size
                     entriesNamesAndSizes.add(Pair(name, size))
-                    Log.v("Applog", "entry name: $name - ${numberFormat.format(size)}")
+//                    Log.v("Applog", "entry name: $name - ${numberFormat.format(size)}")
                 }
-                val endTime = System.currentTimeMillis()
-                Log.d("AppLog", "got ${entriesNamesAndSizes.size} entries data. time taken: ${endTime - startTime}ms")
-                return true
+                val timeTaken = System.currentTimeMillis() - startTime
+//                Log.d("AppLog", "got ${entriesNamesAndSizes.size} entries data. time taken: $timeTaken ms")
+                return ParsingResult(entriesNamesAndSizes, timeTaken)
 
             }
         } catch (e: Throwable) {
-            Log.e("AppLog", "error while trying to parse using Apache ZipFile:$e")
+//            Log.e("AppLog", "error while trying to parse using Apache ZipFile:$e")
             e.printStackTrace()
         }
-        return false
+        return null
     }
 
-    private fun parseUsingZipInputStream(file: File): Boolean {
-        Log.d("AppLog", "testing file using ZipInputStream")
+    private fun parseUsingZipInputStream(file: File): ParsingResult? {
+//        Log.d("AppLog", "testing file using ZipInputStream")
         try {
             val startTime = System.currentTimeMillis()
             ZipInputStream(FileInputStream(file)).use {
@@ -133,21 +159,21 @@ class MainActivity : AppCompatActivity() {
                     val name = entry.name
                     val size = entry.size
                     entriesNamesAndSizes.add(Pair(name, size))
-                    Log.v("Applog", "entry name: $name - ${numberFormat.format(size)}")
+//                    Log.v("Applog", "entry name: $name - ${numberFormat.format(size)}")
                 }
-                val endTime = System.currentTimeMillis()
-                Log.d("AppLog", "got ${entriesNamesAndSizes.size} entries data. time taken: ${endTime - startTime}ms")
-                return true
+                val timeTaken = System.currentTimeMillis() - startTime
+//                Log.d("AppLog", "got ${entriesNamesAndSizes.size} entries data. time taken: $timeTaken ms")
+                return ParsingResult(entriesNamesAndSizes, timeTaken)
             }
         } catch (e: Throwable) {
-            Log.e("AppLog", "error while trying to parse using Apache ZipFile:$e")
+//            Log.e("AppLog", "error while trying to parse using Apache ZipFile:$e")
             e.printStackTrace()
         }
-        return false
+        return null
     }
 
-    private fun parseUsingApacheZipArchiveInputStream(file: File): Boolean {
-        Log.d("AppLog", "testing with ZipArchiveInputStream")
+    private fun parseUsingApacheZipArchiveInputStream(file: File): ParsingResult? {
+//        Log.d("AppLog", "testing with ZipArchiveInputStream")
         try {
             val startTime = System.currentTimeMillis()
             ZipArchiveInputStream(FileInputStream(file)).use {
@@ -157,21 +183,21 @@ class MainActivity : AppCompatActivity() {
                     val name = entry.name
                     val size = entry.size
                     entriesNamesAndSizes.add(Pair(name, size))
-                    Log.v("Applog", "entry name: $name - ${numberFormat.format(size)}")
+//                    Log.v("Applog", "entry name: $name - ${numberFormat.format(size)}")
                 }
-                val endTime = System.currentTimeMillis()
-                Log.d("AppLog", "got ${entriesNamesAndSizes.size} entries data. time taken: ${endTime - startTime}ms")
-                return true
+                val timeTaken = System.currentTimeMillis() - startTime
+//                Log.d("AppLog", "got ${entriesNamesAndSizes.size} entries data. time taken: $timeTaken ms")
+                return ParsingResult(entriesNamesAndSizes, timeTaken)
             }
         } catch (e: Throwable) {
-            Log.e("AppLog", "error while trying to parse using ZipArchiveInputStream:$e")
+//            Log.e("AppLog", "error while trying to parse using ZipArchiveInputStream:$e")
             e.printStackTrace()
         }
-        return false
+        return null
     }
 
-    private fun parseUsingApacheZipFileViaByteArray(file: File, fileSize: Long): Boolean {
-        Log.d("AppLog", "testing file using byte array")
+    private fun parseUsingApacheZipFileViaByteArray(file: File, fileSize: Long): ParsingResult? {
+//        Log.d("AppLog", "testing file using byte array")
         try {
             val startTime = System.currentTimeMillis()
             val bytes = file.inputStream().use { it.readBytesWithSize(fileSize)!! }
@@ -181,21 +207,21 @@ class MainActivity : AppCompatActivity() {
                     val name = entry.name
                     val size = entry.size
                     entriesNamesAndSizes.add(Pair(name, size))
-                    Log.v("Applog", "entry name: $name - ${numberFormat.format(size)}")
+//                    Log.v("Applog", "entry name: $name - ${numberFormat.format(size)}")
                 }
-                val endTime = System.currentTimeMillis()
-                Log.d("AppLog", "got ${entriesNamesAndSizes.size} entries data. time taken: ${endTime - startTime}ms")
-                return true
+                val timeTaken = System.currentTimeMillis() - startTime
+//                Log.d("AppLog", "got ${entriesNamesAndSizes.size} entries data. time taken: $timeTaken ms")
+                return ParsingResult(entriesNamesAndSizes, timeTaken)
             }
         } catch (e: Throwable) {
-            Log.e("AppLog", "error while trying to parse using byte array:$e")
+//            Log.e("AppLog", "error while trying to parse using byte array:$e")
             e.printStackTrace()
         }
-        return false
+        return null
     }
 
-    private fun parseUsingSeekableInUriByteChannel(uri: Uri): Boolean {
-        Log.d("AppLog", "testing using SeekableInUriByteChannel (re-creating inputStream when needed) ")
+    private fun parseUsingSeekableInUriByteChannel(uri: Uri): ParsingResult? {
+//        Log.d("AppLog", "testing using SeekableInUriByteChannel (re-creating inputStream when needed) ")
         try {
             val startTime = System.currentTimeMillis()
             ZipFile(SeekableInUriByteChannel(this, uri)).use { zipFile: ZipFile ->
@@ -204,21 +230,21 @@ class MainActivity : AppCompatActivity() {
                     val name = entry.name
                     val size = entry.size
                     entriesNamesAndSizes.add(Pair(name, size))
-                    Log.v("Applog", "entry name: $name - ${numberFormat.format(size)}")
+//                    Log.v("Applog", "entry name: $name - ${numberFormat.format(size)}")
                 }
-                val endTime = System.currentTimeMillis()
-                Log.d("AppLog", "got ${entriesNamesAndSizes.size} entries data. time taken: ${endTime - startTime}ms")
-                return true
+                val timeTaken = System.currentTimeMillis() - startTime
+//                Log.d("AppLog", "got ${entriesNamesAndSizes.size} entries data. time taken: $timeTaken ms")
+                return ParsingResult(entriesNamesAndSizes, timeTaken)
             }
         } catch (e: Throwable) {
-            Log.e("AppLog", "error while trying to parse using SeekableInUriByteChannel:$e")
+//            Log.e("AppLog", "error while trying to parse using SeekableInUriByteChannel:$e")
             e.printStackTrace()
         }
-        return false
+        return null
     }
 
-    private fun parseUsingInefficientSeekableInUriByteChannel(uri: Uri): Boolean {
-        Log.d("AppLog", "testing using InefficientSeekableInUriByteChannel (re-creating inputStream when needed) ")
+    private fun parseUsingInefficientSeekableInUriByteChannel(uri: Uri): ParsingResult? {
+//        Log.d("AppLog", "testing using InefficientSeekableInUriByteChannel (re-creating inputStream when needed) ")
         try {
             val startTime = System.currentTimeMillis()
             ZipFile(InefficientSeekableInUriByteChannel(this, uri)).use { zipFile: ZipFile ->
@@ -227,21 +253,21 @@ class MainActivity : AppCompatActivity() {
                     val name = entry.name
                     val size = entry.size
                     entriesNamesAndSizes.add(Pair(name, size))
-                    Log.v("Applog", "entry name: $name - ${numberFormat.format(size)}")
+//                    Log.v("Applog", "entry name: $name - ${numberFormat.format(size)}")
                 }
-                val endTime = System.currentTimeMillis()
-                Log.d("AppLog", "got ${entriesNamesAndSizes.size} entries data. time taken: ${endTime - startTime}ms")
-                return true
+                val timeTaken = System.currentTimeMillis() - startTime
+//                Log.d("AppLog", "got ${entriesNamesAndSizes.size} entries data. time taken: $timeTaken ms")
+                return ParsingResult(entriesNamesAndSizes, timeTaken)
             }
         } catch (e: Throwable) {
-            Log.e("AppLog", "error while trying to parse using InefficientSeekableInUriByteChannel:$e")
+//            Log.e("AppLog", "error while trying to parse using InefficientSeekableInUriByteChannel:$e")
             e.printStackTrace()
         }
-        return false
+        return null
     }
 
-    private fun parseUsingJniByteArray(file: File): Boolean {
-        Log.d("AppLog", "testing file using JNI byte array")
+    private fun parseUsingJniByteArray(file: File): ParsingResult? {
+//        Log.d("AppLog", "testing file using JNI byte array")
         try {
             val startTime = System.currentTimeMillis()
 //                Log.d("AppLog", "file size:${numberFormat.format(fileSize)} fileSizeOnDisk:${numberFormat.format(fileSizeOnDisk)} fileSizeFromUri:${numberFormat.format(fileSizeFromUri)}")
@@ -265,21 +291,21 @@ class MainActivity : AppCompatActivity() {
                     val name = entry.name
                     val size = entry.size
                     entriesNamesAndSizes.add(Pair(name, size))
-                    Log.v("Applog", "entry name: $name - ${numberFormat.format(size)}")
+//                    Log.v("Applog", "entry name: $name - ${numberFormat.format(size)}")
                 }
             }
 //                printMemStats(this)
             jniByteArrayHolder.freeBuffer(byteBuffer)
 //                Log.d("AppLog", "memory after freeing")
 //                printMemStats(this)
-            val endTime = System.currentTimeMillis()
-            Log.d("AppLog", "got ${entriesNamesAndSizes.size} entries data. time taken: ${endTime - startTime}ms")
-            return true
+            val timeTaken = System.currentTimeMillis() - startTime
+//            Log.d("AppLog", "got ${entriesNamesAndSizes.size} entries data. time taken: $timeTaken ms")
+            return ParsingResult(entriesNamesAndSizes, timeTaken)
         } catch (e: Exception) {
-            Log.e("AppLog", "error while trying to parse using JNI byte array:$e")
+//            Log.e("AppLog", "error while trying to parse using JNI byte array:$e")
             e.printStackTrace()
         }
-        return false
+        return null
     }
 
     companion object {
